e47f4fac01c6bc1485d08521936b33f9bb6db5ce
Evgeniy <evgeniy@dell-Inspiron-N5110.(none)>
2013-12-05 17:25:47 +0200

    web

--- web/preparation.web.js
+++ web/preparation.web.js
@@ -1,16 +1,7 @@
 var csssrc = document.getElementById('csssrc'),
     cssdst = document.getElementById('cssdst'),
-    slength = document.getElementById('slength'),
-    dlength = document.getElementById('dlength'),
-    result = document.getElementById('result');
 
-function updateStat() {
-    slength.innerHTML = csssrc.value.length;
-    dlength.innerHTML = cssdst.value.length;
-    result.innerHTML = (100 * cssdst.value.length / csssrc.value.length).toFixed(2);
-}
-
-document.getElementById('minimize').onclick = function() {
+document.getElementById('prepare').onclick = function() {
  
     prepareFile = function(data) {
         return _toLowerCase(replaceQuotes(data));
